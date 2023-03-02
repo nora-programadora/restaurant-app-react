4ab6d8ad03851e4abb1aef9084e35d43e1a96cd4
Nora-programadora <nora.programadora@gmail.com>
2023-03-01 22:05:34 -0600

    Bootstrap, axios, setState, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
+import { Button, Card, Container, Row } from 'react-bootstrap'
 import './App.css'
 import Hello from './components/hello'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0);
+  const [joke, setJoke] = useState();
+
+  const getJoke = async () => {
+    const url = 'https://api.chucknorris.io/jokes/random';
+    const response = await axios.get(url);
+    console.log('joke ', response);
+    setJoke(response.data);
+  }
+
+  useEffect(() => {
+    getJoke()
+  }, []);
+
+  const reservaciones = [
+    {
+      nombre: 'Fatima',
+      edad: 25
+    },
+    {
+      nombre: 'Rebeca',
+      edad: 24
+    },
+    {
+      nombre: 'Alan',
+      edad: 29
+    },
+    {
+      nombre: 'Rita',
+      edad: 29
+    },
+
+  ]
 
   return (
     <div className="App">
@@ -18,9 +53,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,6 +64,39 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+        <Container>
+          <Row>
+          {
+            reservaciones.map((item, index) => (
+              <Card key={index} style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Reservacion {item.nombre}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{item.edad}</Card.Subtitle>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+              </Card>
+            ))
+          }
+          </Row>
+        </Container>
+        <Container>
+        <Card>
+          <Card.Header>Chuk Norris API</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Container>
     </div>
   )
 }
